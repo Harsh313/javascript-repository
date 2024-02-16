83822cea6bbc051f27d1917784a8960025e890b0
Harsh313 <91689978+Harsh313@users.noreply.github.com>
2024-02-16 08:03:14 +0000

    conversion and operations on data types

--- 01_basic/03_conversionOpertion.js
+++ 01_basic/03_conversionOpertion.js
@@ -1,16 +1,43 @@
 let score =null
-console.log(typeof score);
+//console.log(typeof score);
 
 
 let valueNumber = Number(score);
-console.log(typeof valueNumber);
-console.log(valueNumber);//NaN output  if score is "123abc" and if score is null output is 0
+//console.log(typeof valueNumber);
+//console.log(valueNumber);//NaN output  if score is "123abc" and if score is null output is 0
 
 
 let isloggedin = 1
 let boolisloggedin = Boolean(isloggedin);
-console.log(typeof boolisloggedin);
-console.log(boolisloggedin);
+//console.log(typeof boolisloggedin);
+//console.log(boolisloggedin);
 
+
+
+
+// ************************operation*************************//
+let num =3
+let negnum=-num
+console.log(negnum);
+
+let str1="Hello";
+let str2 = " Harsh"
+let str3 =str1+str2
+//console.log(str3);
+/*console.log(1+"2")//12
+console.log("1"+2);//12
+console.log("1"+"2" );//12
+console.log("1"+2+2);//122
+console.log(1+2 + "2");//32
+*/
+
+
+//tricky conversion
+// console.log(+true);output - 1
+// console.log(+"");output - 0
+let counter =100
+let counter1=100
+console.log(counter++);//not change 100 will be printed
+console.log(++counter1);//change before printing(operation) 101 is printed
 
 
