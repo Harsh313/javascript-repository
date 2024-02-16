d37d142012c60c9e5d5447884adff061eb5d5b56
Harsh313 <91689978+Harsh313@users.noreply.github.com>
2024-02-16 06:42:53 +0000

    Data types in javascript

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict";//treat all code as new JS code as a newer version
+// alert(3+2)   we are using nodejs not a browser so this statment is not valid
+
+let name = "Harsh"
+let age = 18
+let isloggedIn = false
+
+// premetive data type
+// number =>2 to the power 53
+// bigint 
+// string =>""
+// boolean true/false
+// null => standalone value--- null is a object whereas undefined is a datatype
+// undefined =>
+// symbol => unique
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isloggedIn);
+console.log(typeof undefined);// undefined as a output
+console.log(typeof null);//  object as a output
+
+//object 
+
+
