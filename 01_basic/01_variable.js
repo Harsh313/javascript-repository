0fe585d202f30e8f107be0fe3c81734dbf80d218
Harsh313 <91689978+Harsh313@users.noreply.github.com>
2024-02-16 06:17:51 +0000

    git commit
    git log
    
    git push origin main

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,8 @@
+const account_id =12345;
+let email="harshgmail.com";
+var password ="456";
+accountcity="Delhi";//not prefered don't use this method
+let account_state;//if we don't give any value to a variable then it is characterised as undefined
+//account_id=2; Not allowed
+console.log(account_id);
+console.table([accountcity,email,password,account_state]);
