62da523708f1d93d86654d6f3cfab28838ab422a
Harsh313 <91689978+Harsh313@users.noreply.github.com>
2024-02-16 07:41:06 +0000

    conversionOperation

---
+++ 01_basic/03_conversionOpertion.js
@@ -0,0 +1,16 @@
+let score =null
+console.log(typeof score);
+
+
+let valueNumber = Number(score);
+console.log(typeof valueNumber);
+console.log(valueNumber);//NaN output  if score is "123abc" and if score is null output is 0
+
+
+let isloggedin = 1
+let boolisloggedin = Boolean(isloggedin);
+console.log(typeof boolisloggedin);
+console.log(boolisloggedin);
+
+
+
