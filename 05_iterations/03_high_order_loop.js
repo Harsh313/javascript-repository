fe8b417f19d6bb3efbe0ed451360bc8f334aa689
Harsh313 <91689978+Harsh313@users.noreply.github.com>
2024-02-20 04:22:54 +0000

    for of loop in js

---
+++ 05_iterations/03_high_order_loop.js
@@ -0,0 +1,37 @@
+// for of 
+
+// const arr = [1,2,3,4,5]
+
+// for (const num of arr) {
+//     console.log(num);
+// }
+// let greetings = "Hello World"
+
+// for (const greet of greetings) {
+//     console.log(`Each char is ${greet}`);
+// }
+// for of can be use on any iteration type variable like string,array,objects
+
+
+// maps in javascript
+
+const map = new Map()
+map.set("IN","India")
+map.set("USA","united state of america")
+map.set("FRA","france")
+// console.log(map);
+
+// for (const [key,value] of map) {// first we have to destructure the array then we can use key and its value separately
+//     console.log(key);
+// }
+
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);   // object cannot be iteratable like this 
+    
+// }
